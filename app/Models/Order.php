1dadb356a80ba060d0446b6fc6f3ac2e16c0375b
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-10 13:35:09 +0200

    create Order model, factory and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    private int $day;
+    private float $total;
+    private int $quantity;
+    private float $productPrice;
+    private string $notes;
+    private OrderStatus $status;
+
+    use HasFactory;
+}
+
+enum OrderStatus: string
+{
+    case OVERDUE = 'overdue';
+    case PENDING = 'pending';
+    case PAID = 'paid';
+}
---
+++ database/migrations/2024_07_10_112612_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->decimal('total', 10, 2)->unsigned();
+            $table->unsignedTinyInteger('quantity');
+            $table->decimal('product_price', 10, 2)->unsigned();
+            $table->string('notes');
+            $table->enum('status', ['overdue', 'pending', 'paid']);
+            $table->datetimes();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_0900_ai_ci');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
