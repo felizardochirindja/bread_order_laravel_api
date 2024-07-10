74478b178a00edf6a1891c9a8fefe5efa3c0265c
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-10 13:06:38 +0200

    make name unique

--- database/migrations/2024_07_09_131101_create_months_table.php
+++ database/migrations/2024_07_09_131101_create_months_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('months', function (Blueprint $table) {
             $table->unsignedTinyInteger('id')->autoIncrement()->primary();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->datetimes();
             $table->charset('utf8mb4');
             $table->collation('utf8mb4_0900_ai_ci');
