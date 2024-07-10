b19616b4958add24111d395846bbd2194923ace8
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-10 13:40:27 +0200

    rename Order to DailyOrder

--- app/Models/DailyOrder.php
+++ app/Models/DailyOrder.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class DailyOrder extends Model
 {
     private int $day;
     private float $total;
