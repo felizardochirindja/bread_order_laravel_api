9357a3b2913bbe604856673a07af35a79d390336
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-15 16:27:46 +0200

    create seeder to payment, daily_orders,order_payments tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\DailyOrder;
 use App\Models\MonthlyOrder;
+use App\Models\Payment;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,5 +19,52 @@ class DatabaseSeeder extends Seeder
         Product::factory(1)->create();
         MonthlyOrder::factory(1)->create();
         DailyOrder::factory(5)->create();
+        Payment::factory(2)->create();
+
+        DB::table('daily_orders')->insert([
+            [
+                'monthly_order_id' => 1,
+                'order_id' => 1,
+            ],
+            [
+                'monthly_order_id' => 1,
+                'order_id' => 2,
+            ],
+            [
+                'monthly_order_id' => 1,
+                'order_id' => 3,
+            ],
+            [
+                'monthly_order_id' => 1,
+                'order_id' => 4,
+            ],
+            [
+                'monthly_order_id' => 1,
+                'order_id' => 5,
+            ],
+        ]);
+
+        DB::table('order_payments')->insert([
+            [
+                'daily_order_id' => 1,
+                'payment_id' => 1,
+            ],
+            [
+                'daily_order_id' => 2,
+                'payment_id' => 1,
+            ],
+            [
+                'daily_order_id' => 3,
+                'payment_id' => 1,
+            ],
+            [
+                'daily_order_id' => 4,
+                'payment_id' => 2,
+            ],
+            [
+                'daily_order_id' => 5,
+                'payment_id' => 2,
+            ],
+        ]);
     }
 }
