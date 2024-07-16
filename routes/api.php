6a9fc8909b81d0d5df12deb1ad51aa4984f11463
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 15:57:43 +0200

    refactor

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\V1\DailyOrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\V1'], function() {
-    Route::apiResource('daily-orders', DailyOrderController::class)->only([
-        'index', 'show', 'store'
+    Route::apiResource('daily-orders', DailyOrderController::class)->except([
+        'update'
     ]);
 
-    Route::put('daily-orders/{id}', 'DailyOrderController@update');
+    Route::put('daily-orders/{id}', [DailyOrderController::class, 'update']);
 });
