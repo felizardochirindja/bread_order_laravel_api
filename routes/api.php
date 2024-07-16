aa79bcfe1529bf27307b0acde94cb68b92189173
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 09:58:56 +0200

    specify accepted methods on api resource

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\API\V1\DailyOrderController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\API\V1'], function() {
-    Route::apiResource('daily-orders', DailyOrderController::class);
+    Route::apiResource('daily-orders', DailyOrderController::class)->only([
+        'index', 'show', 'store'
+    ]);
 });
