0a44bb1181658a9979376a8725fda66826b04a06
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 15:20:15 +0200

    ref: rename resources

--- app/Http/Controllers/API/V1/DailyOrderController.php
+++ app/Http/Controllers/API/V1/DailyOrderController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreDailyOrderRequest;
 use App\Http\Requests\V1\UpdateDailyOrderRequest;
-use App\Http\Resources\V1\DailyOrderCollection;
-use App\Http\Resources\V1\DailyOrderResource;
+use App\Http\Resources\V1\ListDailyOrderResource;
+use App\Http\Resources\V1\ReadDailyOrderResource;
 use App\Http\Resources\V1\StoreDailyOrderResource;
 use App\Http\Resources\V1\UpdateDailyOrderResource;
 use App\Models\DailyOrder;
@@ -15,12 +15,12 @@ class DailyOrderController extends Controller
 {
     public function index()
     {
-        return new DailyOrderCollection(DailyOrder::all());
+        return new ListDailyOrderResource(DailyOrder::all());
     }
 
     public function show(DailyOrder $dailyOrder)
     {
-        return new DailyOrderResource($dailyOrder);
+        return new ReadDailyOrderResource($dailyOrder);
     }
 
     public function store(StoreDailyOrderRequest $request)
