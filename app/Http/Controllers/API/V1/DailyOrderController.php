46086f4b982e70feb8c26cb63763136675523417
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 09:59:10 +0200

    implement store daily order

--- app/Http/Controllers/API/V1/DailyOrderController.php
+++ app/Http/Controllers/API/V1/DailyOrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreDailyOrderRequest;
 use App\Http\Resources\V1\DailyOrderCollection;
 use App\Http\Resources\V1\DailyOrderResource;
 use App\Models\DailyOrder;
@@ -16,6 +17,13 @@ class DailyOrderController extends Controller
 
     public function show(DailyOrder $dailyOrder)
     {
+        return new DailyOrderResource($dailyOrder);
+    }
+
+    // create a daily order
+    public function store(StoreDailyOrderRequest $request)
+    {
+        $dailyOrder = DailyOrder::create($request->all());
         return new DailyOrderResource($dailyOrder);
     }
 }
---
+++ app/Http/Requests/V1/StoreDailyOrderRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreDailyOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'day' => ['required'],
+            'total' => ['required'],
+            'quantity' => ['required', 'numeric'],
+            'productPrice' => ['required'],
+            'notes' => ['required'],
+            'status' => ['required', Rule::in(['overdue', 'pending', 'paid'])],
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'product_price' => $this->productPrice,
+        ]);
+    }
+}
