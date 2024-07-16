56209dc3cd3931be6465841b4476e44ad3cede9f
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 06:39:52 +0200

    create daily order resources

---
+++ app/Http/Resources/V1/DailyOrderCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class DailyOrderCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'status' => 'OK',
+            'message' => 'data read successfully',
+            'data' => $this->collection->transform(function($dailyOrder) {
+                return [
+                    'id' => $dailyOrder->id,
+                    'total' => $dailyOrder->total,
+                    'quantity' => $dailyOrder->quantity,
+                    'productPrice' => $dailyOrder->product_price,
+                    'notes' => $dailyOrder->notes,
+                    'day' => $dailyOrder->day,
+                    'status' => $dailyOrder->status,
+                ];
+            }),
+        ];
+    }
+}
