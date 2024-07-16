2e40e70d6712ebcc5f097c636e44f2daeab9ca33
felizardochirindja <felizardo.chirindja@gmail.com>
2024-07-16 15:53:54 +0200

    move daily order controller out of api folder

--- app/Http/Controllers/V1/DailyOrderController.php
+++ app/Http/Controllers/V1/DailyOrderController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API\V1;
+namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreDailyOrderRequest;
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\API\V1\DailyOrderController;
+use App\Http\Controllers\V1\DailyOrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\API\V1'], function() {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\V1'], function() {
     Route::apiResource('daily-orders', DailyOrderController::class)->only([
         'index', 'show', 'store'
     ]);
